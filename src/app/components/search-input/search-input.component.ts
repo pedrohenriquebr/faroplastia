b7a3ef5302a6b4d965641d48f262f818fb374746
pedrobr <pedro.braga@targetbank.com.br>
2022-05-31 23:43:26 -0300

    Create SearchInputComponent

--- src/app/components/search-input/search-input.component.ts
+++ src/app/components/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-input',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchInputComponent implements OnInit {
 
+  @Output()
+  search = new EventEmitter<string>();
+
+  searchText: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onChange(e:Event){
+   if(e.target){
+     console.log((e.target as HTMLInputElement).value);
+     this.searchText = (e.target as HTMLInputElement).value;
+     this.search.emit(this.searchText);
+   }
   }
 
 }
